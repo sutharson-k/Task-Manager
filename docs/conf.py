f4ee9750cf1ab307c4f463ca65253b26f6b48b08
sutharson <sutharsonmohan@gmail.com>
2026-02-07 23:05:59 +0530

    Fix CI: Format conf.py with black

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,13 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Professional Task Manager'
-copyright = '2026'
-author = ''
+sys.path.insert(0, os.path.abspath("."))
+
+project = "Professional Task Manager"
+copyright = "2026"
+author = ""
 extensions = []
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
